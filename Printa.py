acad8023ddb29ea0cfc1b156ff565ebaa397767e
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-04-18 19:54:46 -0500

    Add files via upload
    hi hi hi hi. My first code

---
+++ Printa.py
@@ -0,0 +1,42 @@
+print("oladesire")
+print("she is smart")
+print("she is beautiful")
+print("she is kind")
+print("she is humble")
+print("she is loving")
+print("she is caring")
+print("she is generous")
+print("she is honest")
+print("she is loyal")
+print("she is hardworking")
+print("she is intelligent")
+print("she is creative")
+print("she is talented")
+print("she is funny")
+print("she is friendly")
+print("she is outgoing")
+print("she is confident")
+print("she is ambitious")
+print("she is determined")
+print("she is passionate")
+print("she is optimistic")
+print("she is positive")
+print("she is supportive")
+print("she is understanding")
+print("she is patient")
+print("she is forgiving")
+print("she is respectful")
+p
+x=5
+print(x)
+y=10
+print(y)
+z=x+y
+print(z)
+a=x-y
+print(a)
+b=x*y
+print(b)
+c=x/y
+import keyword
+print(keyword.kwlist)
