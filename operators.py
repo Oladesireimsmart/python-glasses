478beaacfcdb0b5497590062e0646625a99db408
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-04-25 19:30:53 -0500

    Add files via upload
    bye

---
+++ operators.py
@@ -0,0 +1,16 @@
+a=3567
+b=2890
+c=6457
+d=8457
+e=20035
+f=100890
+g=1000689
+sum=a+b+c+d+e+f+g
+print(sum)
+
+difference=a-b-c-d-e-f-g
+print(difference)
+multiplacation=a*b*c*d*e*f*g
+print(multiplacation)
+division=a/b/c/d/e/f/g
+print(division)
