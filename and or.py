1752a89fd50281083a303a375fa39ae4e83258c9
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-05-02 19:25:49 -0500

    Add files via upload
    no thank you

---
+++ and or.py
@@ -0,0 +1,15 @@
+a = 14
+b = 15
+
+if a<200 and b<560:
+    print("Condition passed")
+else:
+    print("Condition failed")
+
+c = 10
+d = -10
+
+if c>0 or d>0:
+    print("Condition passed")
+else:
+    print("Condition failed")
