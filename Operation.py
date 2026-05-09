1ec8e6491675fb1e7f7a14f648f052f94e2e36f4
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-05-08 19:35:10 -0500

    Add files via upload

---
+++ Operation.py
@@ -0,0 +1,30 @@
+x = 100
+
+if(type(x) is int):
+ print("True")
+else: 
+ print("False")
+
+
+x = 5.5
+
+if(type(x) is not float):
+ print("True")
+else:
+ print("False")
+
+x = 2000
+y = 3000
+
+if(x is y):
+ print("x and y same identity")
+else:
+ print("x and y different identities")
+
+
+
+
+
+
+
+    
