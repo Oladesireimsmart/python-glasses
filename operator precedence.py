40a4dc6171fe537d65c9800f7de7b6b528342ae2
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-05-09 19:39:39 -0500

    Add files via upload

---
+++ operator precedence.py
@@ -0,0 +1,28 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w)*x/y
+print("Value of (v+w)*x/y is: ", z)
+
+
+
+name = "OLADESIRE"
+age = 8
+
+if name == "OLADESIERE" or name == "LORE" and age >=2 :
+    print("Hello welcome")
+else:
+    print("Sorry, you are not welcome")   
+
+
+
+
+
+n1 = 10
+n2 = 20 
+ if n1 % n2 == 0
+    print("n1 is divisible by n2")
+ else:
+    print("n1 is not divisible by n2")
