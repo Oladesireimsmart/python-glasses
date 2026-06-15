6e8562bb88500bb8240e6f52ad5b4746a76560c7
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-06-15 13:09:49 -0500

    Add files via upload

---
+++ circumfrencesacp.py
@@ -0,0 +1,10 @@
+def circumference(radius):
+     return 2*3.14* radius
+
+     #input from user
+     r=float(input("Enter the radius of the circle:"))
+
+     # Function call
+     c=circumference
+
+     print("circumference of the circle=",c)
