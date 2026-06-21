4a0e0931740cef43919831c439f047a9c5534b3d
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-06-20 19:46:50 -0500

    Add files via upload

---
+++ doyouamountacp.py
@@ -0,0 +1,6 @@
+bill_amount = int(input("Enter the bill amount: "))
+paid_amount = int(input("Enter the paid amount: "))
+
+due_amount = bill_amount - paid_amount
+
+print("The due amount is:", due_amount)
