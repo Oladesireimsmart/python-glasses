8022b322676af42fdaca334c0dd9eae91da02bfd
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-06-27 19:37:33 -0500

    Add files via upload

---
+++ classacp.py
@@ -0,0 +1,13 @@
+def age_counter(age):
+    if age < 18:
+        print("You are a minor.")
+    elif age < 65:
+        print("You are an adult.")
+    else:
+        print("You are a senior citizen.")
+
+
+        user_age = int(input("Enter your age: "))
+
+
+        age_counter(user_age)
