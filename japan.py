a9cf3fc917d84f857c575815fcd79f1e09872a0f
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-07-18 19:27:44 -0500

    Add files via upload

---
+++ japan.py
@@ -0,0 +1,8 @@
+country_code={'india':'oo01','japan':'oo02','usa':'oo03'}
+
+
+print(country_code['japan'])
+print(country_code.get('japan', 'NOT FOUND'))
+
+
+print("country_code['india']:",country_code['india'])
