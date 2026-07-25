986470958acfa92ebc56beabe2c4de4ef276a8e2
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-07-24 19:36:05 -0500

    Add files via upload

---
+++ acp.py
@@ -0,0 +1,13 @@
+snacks={" chips", " cookies", " candy", " ice cream"}
+snacks.add(" chocolate")
+
+other_snacks={" cookies", " candy", " nuts", " popcorn"}
+common_snacks = snacks.intersection(other_snacks)
+
+counts=[2, 3,1]
+counts.append(4)
+
+print("The common snacks are:", common_snacks)
+
+counts.reverse()
+print("The counts are:", counts)
