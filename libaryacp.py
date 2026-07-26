fb3f85ee86fb90ae1620c190f4b3da61d3e0b7db
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-07-25 19:44:59 -0500

    Add files via upload

---
+++ libaryacp.py
@@ -0,0 +1,9 @@
+books=["Maths","English","Biology","Chemistry","Physics"]
+copies=[3, 0, 2, 1]
+
+libary=list(zip(books, copies))
+
+print("The libary is:", libary)
+print(libary)
+
+choice=input("Enter the book you want to check:")
