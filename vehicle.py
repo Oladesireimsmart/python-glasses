713c9273ec2c78caf9f7c17d55ba2d5317a52640
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-08-01 19:25:55 -0500

    Add files via upload

---
+++ vehicle.py
@@ -0,0 +1,15 @@
+class Vechile:
+    
+    
+      def __init__(self, max_speed, color, ):
+            
+
+        self.max_speed = max_speed
+        self.color = color
+        
+
+modelx = Vechile(240, 18)
+
+
+print("The max speed of the vechile is:", modelx.max_speed)
+print("The color of the vechile is:", modelx.color)
