7216983686a59f113540c6ce61a125913ea423e6
Oladesireimsmart <oladesireolatunde@gmail.com>
2026-08-22 19:45:25 -0500

    Add files via upload

---
+++ pygame.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((400, 500))
+
+done = False
+
+while not done:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill((255, 255, 255))
+    pygame.display.flip()
+
+    pygame.time.delay(100)
